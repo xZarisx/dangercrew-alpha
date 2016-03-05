330bbedba9dcb9e1999032815903fbd92a03a22a
Drew Conley <drewconley@gmail.com>
2016-03-04 20:52:31 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: {
+        "dangercrew.alpha": [
+            'webpack-dev-server/client?http://localhost:8080',
+            'webpack/hot/only-dev-server',
+            './src/index.js'
+        ]
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js']
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['react-hot-loader', 'babel-loader'], exclude: /node_modules/ }
+            { test: /\.scss$/, loaders: ["style", "css", "sass"] }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require("path");
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+
+
+var config = {
+    entry: {
+        "dangercrew.alpha": [
+            './src/index.js'
+        ]
+    },
+    output: {
+        path: __dirname + '/dist/',
+        filename: '[name].bundle.js',
+        publicPath: '/dist/'
+    },
+        plugins: [
+            //new webpack.NoErrorsPlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: true
+                },
+                mangle: true
+                //except: ['$', 'exports', 'require']
+
+            })
+        ],
+    resolve: {
+        //extensions: ['', '.js'],
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ },
+            //{ test: /\.scss$/, loaders: ["style", "css", "sass"] }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
+
+
+module.exports = config;
