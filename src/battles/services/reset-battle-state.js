8dc17ae7bc00e18a54977d088d4d419e06a8f254
Drew Conley <drewconley@gmail.com>
2016-03-06 08:45:25 -0600

    fix resetting issues after losing battle

--- src/battles/services/reset-battle-state.js
+++ src/battles/services/reset-battle-state.js
@@ -4,6 +4,27 @@ export default function() {
     /* Dispatch some things to start a fresh battle */
     [
         {
+            type: "CLEAR_BATTLE_SUBMISSIONS"
+        },
+        {
+            type: "SET_ROLLOUT",
+            payload: {
+                rollout: []
+            }
+        },
+        {
+            type: "SET_ROLLOUT_INDEX",
+            payload: {
+                rolloutIndex: 0
+            }
+        },
+        {
+            type: "SET_TEXT_MESSAGE",
+            payload: {
+                textMessage: null
+            }
+        },
+        {
         type: "ADD_STYLE_TO_COMBATANT",
         payload: {
             key: "a",
